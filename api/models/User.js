0ac91e9ba96903f41ce417070f71d37877fad311
Vaibhav Kohli <vaibhav_kohli@external.mckinsey.com>
2014-03-25 18:55:46 +0530

    sails and nodejs yay!!

---
+++ api/models/User.js
@@ -0,0 +1,22 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	name: {
+    type: 'string',
+    maxLength: 20,
+    minLength: 5
+  }
+
+    
+  }
+
+};
